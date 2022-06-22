b8088c569e547e1d3e07a9b7953fbc05a709a842
Werner Bucher <werner.bucher@sicoob.com.br>
2022-06-22 19:35:44 -0300

    Commit Inicial

---
+++ src/main/java/br/com/itau/pix/CadastroDeChavesPixApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeChavesPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeChavesPixApplication.class, args);
+	}
+
+}
